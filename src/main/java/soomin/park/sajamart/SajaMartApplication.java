0f07aac5b20f20bb2792020c270d4a6071e1b028
soomin.park <parksoopark@gmail.com>
2025-03-20 22:58:50 +0900

    최초커밋

---
+++ src/main/java/soomin/park/sajamart/SajaMartApplication.java
@@ -0,0 +1,13 @@
+package soomin.park.sajamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajaMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajaMartApplication.class, args);
+	}
+
+}
